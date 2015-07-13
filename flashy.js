2b40f4c9a19a7c02a991d9e631f81ea5502ed37d
Robert <i.am.robert.benjamin@gmail.com>
2015-07-13 12:21:32 -0400

    Introduce card array / object structure
    Experimented more with the JS. I think the included nested array /
    object is what I’ll use to keep track of the necessary information,
    it’s very organized and easy to access and edit.

--- flashy.js
+++ flashy.js
@@ -1,7 +1,56 @@
-$("polygon").eq(0).click(function() {
-	$("body").css("background-color", "red");
-})
+// $("polygon").eq(0).click(function() {
+// 	$("body").css("background-color", "red");
+// });
+// $("polygon").eq(1).click(function() {
+// 	$("body").css("background-color", "blue");
+// });
 
-$("polygon").eq(1).click(function() {
-	$("body").css("background-color", "blue");
-})
+var cards = [{
+	correct: null,
+	question: "What is George Washington's last name?",
+	answer: "Washington"
+}, {
+	correct: null,
+	question: "What is a burger made of?",
+	answer: "Bun, beef, and nothing else."
+}, {
+	correct: null,
+	question: "What color is a bumblebee?",
+	answer: "Yellow and black, I think."
+}, {
+	correct: null,
+	question: "When does the world end?",
+	answer: "2028"
+}, {
+	correct: null,
+	question: "Who is the main character of 'Ender's Shadow'?",
+	answer: "Bean"
+}, ];
+
+for (i = 0; i < (cards.length - 1); i++) {
+	$("polygon").eq(1).click(function() {
+		$("body").css("background-color", "blue");
+		console.log(i);
+	});
+}
+
+var side = true;
+
+var flipCard = function(){
+	if (side) {
+		$(".question_text").html(cards[0].question);
+		side = false;
+	} else {
+		$(".question_text").html(cards[0].answer);
+		side = true;
+	}
+}
+
+$(".flip").click();
+
+
+// array.push({
+// 	correct: null,
+// 	question: "Lololol",
+// 	answer: "Lolz"
+// });
